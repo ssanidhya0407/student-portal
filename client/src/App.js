a82fdb8862e443764ee0d48f03dd0f5322c0ace7
ssanidhya0407 <ssanidhya0407@gmail.com>
2024-06-08 08:09:34 +0300

    Fix dependencies and update react-router-dom to v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Hostel from './pages/Hostel';
@@ -8,13 +8,13 @@ import Laundry from './pages/Laundry';
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/login" component={Login} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/hostel" component={Hostel} />
         <Route path="/laundry" component={Laundry} />
         <Route path="/" component={Login} />
-      </Switch>
+      </Routes>
     </Router>
   );
 };
