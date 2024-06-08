0a96736d5997f2fef7783d9e6879d968ad3c9f86
ssanidhya0407 <ssanidhya0407@gmail.com>
2024-06-08 07:48:34 +0300

    Serve React build files and finalise deployment setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +16,13 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/hostel', require('./routes/hostel'));
 app.use('/api/laundry', require('./routes/laundry'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
